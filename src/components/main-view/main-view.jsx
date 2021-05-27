08d9b9e6d95e6a4d31edf7fe946b410882f33e71
Juan-Rodelo <juancamilo849@gmail.com>
2021-05-27 16:07:25 -0500

    Login_Registration 2

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 import { LoginView } from '../login-view/login-view';
-
+import { RegistrationView } from '../registration-view/registration-view';
 
 
 // create the component using the react template and export it to make it available
@@ -16,7 +16,7 @@ class MainView extends React.Component {
     this.state = {
       movies: [],
       //The UI is a function of its state that now is null.
-      selectedMovie: null
+      selectedMovie: null,
       user: null
     }
   }
@@ -33,23 +33,48 @@ class MainView extends React.Component {
       });
   }
 
-  onLoggedIn(user) {
-    this.setState({
-      user
-    });
-  }
-
   setSelectedMovie = newSelectedMovie => {
     this.setState({
       selectedMovie: newSelectedMovie
     });
   }
 
+  onLoggedIn(user) {
+    this.setState({
+      user
+    });
+  }
+
+  onRegister(register) {
+    this.setState({
+      register
+    });
+  }
+
+  onBackClick() {
+    this.setState({
+      selectedMovie: null
+    });
+  }
+
+  toggleRegister = (e) => {
+    e.preventDefault();
+    this.setState({
+      register: !this.state.register
+    })
+  }
+
+
+
   render() {
-    const { movies, selectedMovie } = this.state;
+    const { movies, selectedMovie, register } = this.state;
+
+    if (register) return <RegistrationView onRegister={register => this.onRegister(register)} toggleRegister={this.toggleRegister} />;
 
     if (movies.length === 0) return <div className="main-view"></div>;
-    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+    /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are 
+    *passed as a prop to the LoginView*/
+    if (this.state.user === null) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} toggleRegister={this.toggleRegister} />;
 
     return (
       <div className="main-view">
